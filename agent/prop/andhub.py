100880f7979b0b2265d7d0aeaec141eb759fea23
yana sereda <sereda.yana90@yandex.ru>
2022-04-21 21:05:31 +0300

    1

--- agent/prop/andhub.py
+++ agent/prop/andhub.py
@@ -39,7 +39,42 @@ def propagate_into_andhub(andhub, msg):
         return propagate_into_andhub_from_down(andhub, msg)
 
 def propagate_into_andhub_from_top(andhub, msg):
-    pass
+    # в какого из двух детей его передать?
+    if msg.eid in andhub.leftRW.eid_map.keys():
+        andhub.current_RW_is_left = True
+        return andhub.leftRW, msg
+
+    andhub.current_RW_is_left = False
+    return andhub.rightRW, msg
+
+def create_msg_to_left(and_signature, exemplars_from_right):
+    msg_to_left = Msg1(eid, points)
+    return msg_to_left
+
+def create_msg_to_right(and_signature, exemplars_from_left):
+    msg_to_right = Msg1(eid, points)
+    return msg_to_right
 
 def propagate_into_andhub_from_down(andhub, msg):
-    pass
+    if andhub.current_RW_is_left == True:
+        andhub.left_pre_exemplars = msg.exemplars
+    else:
+        andhub.right_pre_exemplars = msg.exemplars
+
+    if andhub.is_runnable(): # вверх
+        exemplars = andhub.run()
+        msg.exemplars = exemplars
+        return andhub.top_receiver, msg
+    else: #вбок
+        if andhub.current_RW_is_left == True:
+            #если пришло слева, то оправляем вправо
+            andhub.current_RW_is_left = False
+            msg_to_right = create_msg_to_right(andhub.and_signature, exemplars_from_left=msg.exemplars)
+            return andhub.rightRW, msg_to_right
+        else:
+            # если пришло справа, то оправляем влево
+            andhub.current_RW_is_left = True
+            msg_to_left = create_msg_to_left(andhub.and_signature, exemplars_from_right=msg.exemplars)
+            return andhub.leftRW, msg_to_left
+
+
